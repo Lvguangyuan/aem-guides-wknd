db0eb025a97f94d0487613b7bde92400c43232a5
GaryLv <lvgy@hello-tech.com>
2024-06-21 18:59:05 +0800

    update project info

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/SitemapGeneratorImpl.java
@@ -0,0 +1,74 @@
+package com.adobe.aem.guides.wknd.core.services;
+
+import java.util.Optional;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.sitemap.SitemapException;
+import org.apache.sling.sitemap.builder.Sitemap;
+import org.apache.sling.sitemap.builder.Url;
+import org.apache.sling.sitemap.spi.common.SitemapLinkExternalizer;
+import org.apache.sling.sitemap.spi.generator.ResourceTreeSitemapGenerator;
+import org.apache.sling.sitemap.spi.generator.SitemapGenerator;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.aem.wcm.seo.sitemap.PageTreeSitemapGenerator;
+import com.day.cq.wcm.api.Page;
+
+@Component(
+        service = SitemapGenerator.class
+)
+public class SitemapGeneratorImpl extends ResourceTreeSitemapGenerator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SitemapGeneratorImpl.class);
+
+    @Reference
+    private SitemapLinkExternalizer externalizer;
+    @Reference
+    private PageTreeSitemapGenerator defaultGenerator;
+
+    @Override
+    protected void addResource(String name, Sitemap sitemap, Resource resource) throws SitemapException {
+        LOG.error("======I am SitemapGeneratorImpl===========");
+        Page page = resource.adaptTo(Page.class);
+        if (page == null) {
+            LOG.debug("Skipping resource at {}: not a page", resource.getPath());
+            return;
+        }
+        String location = externalizer.externalize(resource);
+        Url url = sitemap.addUrl(location + ".aabb.html");
+        LOG.error("==============debug start================");
+        LOG.error(url.toString());
+        LOG.error("==============debug end================");
+        // add any additional content to the Url like lastmod, change frequency, and so on
+    }
+
+    @Override
+    protected final boolean shouldFollow(Resource resource) {
+        return super.shouldFollow(resource)
+                && Optional.ofNullable(resource.adaptTo(Page.class)).map(this::shouldFollow).orElse(Boolean.TRUE);
+    }
+
+    private boolean shouldFollow(Page page) {
+        // add additional conditions to stop traversing some pages
+        return !defaultGenerator.isProtected(page);
+    }
+
+    @Override
+    protected final boolean shouldInclude(Resource resource) {
+        return super.shouldInclude(resource)
+                && Optional.ofNullable(resource.adaptTo(Page.class)).map(this::shouldInclude).orElse(Boolean.FALSE);
+    }
+
+    private boolean shouldInclude(Page page) {
+        // add additional conditions to stop including some pages
+        return defaultGenerator.isPublished(page)
+                && !defaultGenerator.isNoIndex(page)
+                && !defaultGenerator.isRedirect(page)
+                && !defaultGenerator.isProtected(page);
+    }
+}
+
+
